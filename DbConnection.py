c332aea69ca245b5b0d367f17e50b85060557272
Nageswara Rao <nag.samayam@gmail.com>
2017-12-23 23:42:21 +0530

    Parse XML file and store into MySQL

---
+++ DbConnection.py
@@ -0,0 +1,15 @@
+import MySQLdb
+
+
+class DbConnection:
+
+    @staticmethod
+    def connect():
+        return MySQLdb.connect(host="localhost",  # your host, usually localhost
+                             user="root",  # your username
+                             passwd="",  # your password
+                             db="python_tutorial")
+
+    @staticmethod
+    def close(db):
+        db.close()
--- parse_xml.py
+++ parse_xml.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import MySQLdb
 
 
 class ParseXml:
@@ -14,7 +15,6 @@ class ParseXml:
 
         for listings in root:
             toilet = {'locale': 'en'}
-
             if listings.tag == 'listing':
                 for listing in listings:
                     if listing.tag != 'content':
@@ -31,15 +31,75 @@ class ParseXml:
                                     timings_with_day = attribute.text.split(' ')
                                     timing = {'day': timings_with_day[0], 'status': status}
                                     if timings_with_day[1] != 'Closed':
-                                        timings = timings_with_day[1].split('-')
+                                        timing_list = timings_with_day[1].split('-')
                                         timing['status'] = 'Opened'
-                                        timing['from'] = timings[0]
-                                        timing['to'] = timings[1]
+                                        timing['from'] = timing_list[0]
+                                        timing['to'] = timing_list[1]
                                     timings.append(timing)
                                 toilet['timings'] = timings
-                self.toilets.append(toilet)
+                toilet_id = self.store(toilet)
+                self.toilets.append(toilet_id)
+                # self.toilets.append(toilet) // List of toilets
         return self.toilets
+
+    def store(self, row):
+        db_connection = self.db_connect(self)
+        db_cursor = db_connection.cursor()
+
+        ''' Insert into master table'''
+        image = row.get('image')
+        if image is None:
+            row['image'] = None
+
+        query = "INSERT INTO public_toilets(latitude, longitude, picture, category) " \
+                "VALUES(%s, %s, %s,%s)"
+        args = (row['latitude'], row['longitude'], row['image'], row['category'])
+
+        db_cursor.execute(query=query, args=args)
+        public_toilet_id = db_cursor.lastrowid
+
+        if public_toilet_id:
+            timings = row['timings']
+            for timing in timings:
+                from_time = timing.get('from')
+                to_time = timing.get('to')
+                if from_time is None:
+                    timing['from'] = None
+                if to_time is None:
+                    timing['to'] = None
+
+                ''' Insert into translations table'''
+                translations_query = "INSERT INTO public_toilet_translations(public_toilet_id, locale, title,address, timings) " \
+                                     "VALUES(%s,%s,%s,%s,%s)"
+                translations_args = (public_toilet_id, row['locale'], row['name'], row['address'], str(row['timings']))
+                db_cursor.execute(query=translations_query, args=translations_args)
+
+                ''' Insert into timings table'''
+                timings_query = "INSERT INTO public_toilets_timings(public_toilet_id, locale, day, status, from_time, to_time) " \
+                                "VALUES(%s,%s,%s,%s,%s,%s)"
+                timings_args = (public_toilet_id, row['locale'], timing['day'], timing['status'], timing['from'], timing['to'])
+                db_cursor.execute(query=timings_query, args=timings_args)
+
+        db_connection.commit()
+
+        db_cursor.close()
+        self.db_close(self, db=db_connection)
+
+        return public_toilet_id
+
+    @staticmethod
+    def db_connect(self):
+        return MySQLdb.connect(host="localhost",  # your host, usually localhost
+                               user="root",  # your username
+                               passwd="",  # your password
+                               db="python_tutorial")
+
+    @staticmethod
+    def db_close(self, db):
+        db.close()
 
 
 obj = ParseXml("uploads\sample.xml")
-print(obj.reader())
+toilets = obj.reader()
+for toilet in toilets:
+    print("Toilet ID: "+str(toilet))
