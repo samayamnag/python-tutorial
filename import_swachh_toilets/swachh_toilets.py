e8a725bd35ecbe1f2f976f6f6dd362b2c7b3bc3e
Nageswara Rao <nageswara.rao@janaagraha.org>
2018-06-02 20:05:30 +0530

    Import swachh toilets from CSV

--- import_swachh_toilets/swachh_toilets.py
+++ import_swachh_toilets/swachh_toilets.py
@@ -1,17 +1,31 @@
 from mongoengine import (
-    Document, StringField, DictField, ListField, DateTimeField
+    Document, StringField, DictField, ListField, DateTimeField, IntField, BooleanField, FloatField
 )
 from datetime import datetime
 
 
 class SwachhToilet(Document):
-    name = DictField(required=True)
+    qci_id = StringField(null=True, unique=True)
+    toilet_id = StringField(null=True, unique=True)
     address = StringField(required=True)
+    latitude = FloatField(null=True)
+    longitude = FloatField(null=True)
     coordinates = ListField()
+    state = StringField(null=True)
     city = StringField(required=True)
     category = StringField(null=True)
-    timings = ListField(DictField(required=True))
-    image = DictField(default=None)
+    category_code = StringField(null=True)
+    type = StringField(null=True)
+    opening_time = StringField(null=True)
+    closing_time = StringField(null=True)
+    open_days = StringField(default='All Seven Days')
+    seats = IntField(default=0)
+    gender = StringField(default='Gents and Ladies')
+    child_friendly = BooleanField(default=False)
+    differntly_abled_friedly = BooleanField(default=False)
+    fee_type = StringField(default='Free of Charge')
+    cost = StringField(null=True)
+    image = StringField(null=True)
     timestamp = DateTimeField(default=datetime.now)
     comments = ListField(DictField(), default=None)
 
@@ -30,7 +44,7 @@ class SwachhToilet(Document):
     }
 
     def __str__(self):
-        return self.name['en']
+        return self.qci_id
 
     def __repr__(self):
         return f'Public Toilet: {self.id}'
